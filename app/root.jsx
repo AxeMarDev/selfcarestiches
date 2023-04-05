a4abb766692884f73ed19ac9307a70473dac2bfe
Axell Martinez Avila <niz6@txstate.edu>
2023-04-05 13:29:19 -0500

    started webdeisgn
    Added a header section to the index route

--- app/root.jsx
+++ app/root.jsx
@@ -46,7 +46,6 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1>Hello, {name}</h1>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -7,7 +7,51 @@ export const meta = () => {
   export default function Index() {
     return (
       <div>
-        <h3>Hello from the home page!</h3>
+        <Header />
+        <Body />
+        <Footer />
       </div>
     );
   }
+
+  function Header(){
+    return(
+        <div className="HeaderMain">
+            <Announcement />
+            <NavBar />
+
+        </div>
+    );
+  };
+
+  function NavBar(){
+    return(
+        <div  className="NavBarMain"> 
+            <div className="NavBarInner">
+                <p>navbar</p>
+            </div>
+            
+        </div>
+    );
+  };
+  function Announcement(){
+    return(
+        <div  className="AnnounceMain">
+            <p>announcer</p>
+        </div>
+    );
+  };
+  function Body(){
+    return(
+        <div>
+            <p>body</p>            
+        </div>
+    );
+  };
+  function Footer(){
+    return(
+        <div>
+             <p>footer</p>   
+        </div>
+    );
+  };
